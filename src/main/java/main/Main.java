37de5df14ea4ef2d7afbcf4842040c3f71597def
ClementLusson <clementlusson@gmail.com>
2016-12-02 12:18:20 +0100

    set the map party

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+/**
+ * Created by clem on 02/12/2016.
+ */
+public class Main {
+
+}
---
+++ src/main/java/main/MapCanvas.java
@@ -0,0 +1,10 @@
+/**
+ * Created by clem on 02/12/2016.
+ */
+package main;
+
+
+
+public class MapCanvas {
+
+    }
