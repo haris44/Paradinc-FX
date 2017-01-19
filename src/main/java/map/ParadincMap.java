85ed154c3c8d792f34263c5fad9cd099e80d9324
romain2311FCGB <romain.boutinet@consertotech.pro>
2017-01-19 13:33:43 +0100

    set the continents on map

--- src/main/java/map/ParadincMap.java
+++ src/main/java/map/ParadincMap.java
@@ -13,13 +13,30 @@ public class ParadincMap {
     private              CountryRegion africa;
 
     public ParadincMap() {
-        this.europe = new CountryRegion("EU", BE, GR, LT, PT, BG, ES, LU, RO, CZ, FR, HU, SI, DK, HR, MT, SK, DE, IT, NL, FI, EE, CY, AT, SE, IE, LV, PL, GB, BE, NL, LU, AT, CH, RU,IL, LI, LT, IS, IR);
+       this.europe = new CountryRegion("EU", AX, BE, GR, LT, PT, BG, ES, LU, RO, CZ, FR, HU,
+                SI, DK, HR, MT, SK, DE, IT, NL, FI, EE, CY, AT, SE, IE, LV, PL, GB, BE, NL,
+                LU, AT, CH, RU,IL, LI, LT, IS, SH,AL,AD,AM, AT,BY,BA,BV,GE, GI, GR,GG,IE,IM,JE,
+                KG,LV,MK,MT,MD, MC,ME,TM,SE,NO, SK, SH, RS,GS,SJ,UA, VA,KZ,UZ,AF,TR,IR,AZ);
         this.europe.setColor(Color.LIGHTGREEN);
-        this.america = new CountryRegion("AMERICAS", AI,  GL, AG, AR, AW, BS, BB, BZ, BM, BO, BR, CA, KY, CL, CO, CR, CU, DM, DO, EC, SV, GF, GD, GP, GT, GY, HT, HN, JM, MQ, MX, MS, NI, PA, PY, PE, PR, BL, KN, LC, MF, PM, VC, SR, TT, TC, US, UY, VE, VG, VI);
+
+        this.america = new CountryRegion("AMERICAS", AI,  GL, AG, AR, AW, BS, BB, BZ, BM,
+                BO, BR, CA, KY, CL, CO, CR, CU, DM, DO, EC, SV, GF, GD, GP, GT, GY, HT, HN,
+                JM, MQ, MX, MS, NI, PA, PY, PE, PR, BL, KN, LC, MF, PM, VC, SR, TT, TC, US,
+                UY, VE, VG, VI);
         this.america.setColor(Color.LIGHTCYAN);
-        this.oceanie = new CountryRegion("OCEANIE", AS, AU, BD, BN, BT, CC, CK, CN, CX, FJ, FM, GU, HK, ID, IN, IO, JP, KH, KI, KP, KR, LA, LK, MH, MM, MN, MO, MP, MV, MY, NC, NF, NP, NR, NU, NZ, PF, PG, PH, PK, PN, PW, SB, SG, TH, TK, TL, TO, TV, TW, VN, VU, WF, WS);
+
+        this.oceanie = new CountryRegion("OCEANIE", AS, AU, BD, BN, BT, CC, CK, CN, CX, FJ,
+                FM, GU, HK, ID, IN, IO, JP, KH, KI, KP, KR, LA, LK, MH, MM, MN, MO, MP, MV,
+                MY, NC, NF, NP, NR, NU, NZ, PF, PG, PH, PK, PN, PW, SB, SG, TH, TK, TL, TO,
+                TV, TW, VN, VU, WF, WS,RE,SC,TJ);
         this.oceanie.setColor(Color.LIGHTYELLOW);
-        this.africa = new CountryRegion("EMEA", AF, AX, AL, DZ, AD, AO, AM, AT, AZ, BH, BY, BJ, BA, BW, BV, BF, BI, CM, CV, CF, TD, KM, CD, CG, DJ, GQ, ER,  FK, FO, FI, GA, GM, GE, GH, GI, GR, GG, GW, IQ, IE, IM, CI, JE, JO, KZ, KE, KV, KW, KG, LV, LB, LS, LR, LY, MK, MG, MW, ML, MT, MR, MU, YT, MD, MC, ME, MA, MZ, NA, NE, NG, NO, OM, PK, PS, QA, RE, RW, SH, SM, ST, SA, SN, RS, SC, SL, SK, SO, ZA, GS, SD, SJ, SZ, SE, SY, TJ, TZ, TG, TN, TR, TM, UG, UA, AE, UZ, VA, EH, YE, ZM, ZW);
+
+        this.africa = new CountryRegion("AFRICA",DZ,AO,BH,
+                BJ,BW,BF,BI,CM,CV,CF,TD,KM,CD,CG,DJ,GQ,ER,FK,FO,
+                GA,GM,GH,GW,IQ,CI,JO,KE,KV,KW,
+                LB,LS,LR,LY,MG,MW,ML,MR,MU,YT,MA,MZ,NA,NE,NG,
+                OM,PS,QA,RW,SM,ST,SA,SN,SL,SO,ZA,SD,
+                SZ,SY,TZ,TG,TN,UG,AE,EH,YE,ZM,ZW,EG,GN,SS,ET);
         this.africa.setColor(Color.LIGHTSTEELBLUE);
 
         this.world = WorldBuilder.create()
