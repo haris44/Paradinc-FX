e8f2bed0baa6a2d2cc54fc6b85e2a1b591bf1041
NathanGrimaud <grimaudnathan@gmail.com>
2017-01-21 19:32:09 +0100

    (Added) tweet display in ListView

---
+++ src/main/java/model/actions/Tweet.java
@@ -0,0 +1,41 @@
+package model.actions;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by nathan on 21/01/17.
+ */
+public class Tweet {
+    public Date getDate() {
+        return date;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+
+    private Date date;
+    private String message;
+
+    public Tweet(Date date, String message){
+        this.date  = date;
+        this.message = message;
+
+    }
+    public String toString(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "dd/MM/yyyy hh:mm");
+        return dateFormat.format(date)
+
+                +" : "
+                + this.message;
+
+    }
+
+}
--- src/main/java/views/TimelineView.java
+++ src/main/java/views/TimelineView.java
@@ -1,15 +1,29 @@
 package views;
 
 import controllers.GameController;
+import factory.GameFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.Game;
 import model.Timeline;
+import model.actions.Tweet;
+import model.language.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Observable;
 
 /**
  * Created by nathan on 18/01/17.
@@ -23,6 +37,10 @@ public class TimelineView {
     private Text globalInfection;
     private Text turnCounter;
     private Integer turnNumber = 0;
+    private ListView<Tweet> tweetsView;
+    final private ObservableList<Tweet> tweets;
+
+
     public TimelineView(Stage stage, GameController controller){
 
         this.gameCtrl = controller;
@@ -31,7 +49,7 @@ public class TimelineView {
 
         view = (StackPane) stage.getScene().getRoot().getChildrenUnmodifiable().get(0);
         view.getStylesheets().add("Timeline.css");
-        view.setBackground(new Background(new BackgroundFill(Color.web("#3f3f4f"), CornerRadii.EMPTY, Insets.EMPTY)));
+        view.setBackground(new Background(new BackgroundFill(Color.web("#E0E0E0"), CornerRadii.EMPTY, Insets.EMPTY)));
         view.getChildren().add(grid);
         view.getStyleClass().add("timeline");
 
@@ -53,6 +71,22 @@ public class TimelineView {
         globalInfection = new Text("10 %");
         grid.add(globalInfection,4,2);
 
+        String langName = this.gameCtrl.getGame().getLanguage().getName();
+        Tweet welcome = new Tweet(new Date(), "Un nouveau langage vient d'apparaitre : " + langName);
+        Text tweetsLabel = new Text("Tweets sur votre langage :");
+        tweetsLabel.getStyleClass().add("title");
+        grid.add(tweetsLabel,0,3);
+        tweets = FXCollections.observableArrayList(welcome);
+        tweetsView = new ListView<Tweet>(tweets);
+        tweets.addListener((ListChangeListener<Tweet>) change -> {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    System.out.println("++Ajout");
+                    change.getAddedSubList().forEach(value -> System.out.printf("\"%s\" a été ajouté.", value).println());
+                }
+            }
+        });
+        grid.add(tweetsView,0,4);
         stage.getScene().getStylesheets().add(
                 getClass().getResource("../Timeline.css").toExternalForm()
         );
@@ -65,6 +99,10 @@ public class TimelineView {
         turnNumber += 1;
         turnCounter.setText(turnNumber.toString());
         starsCounter.setText(gameCtrl.getGame().getStars().toString());
+        if(turnNumber%10 == 0){
+            Tweet tenTurns = new Tweet(new Date(),"Le langage a passé 10 tours de plus !");
+            this.tweets.add(tenTurns);
+        }
     }
     public GridPane createGrid(){
         GridPane grid = new GridPane();
@@ -72,5 +110,21 @@ public class TimelineView {
         grid.setVgap(20);
         grid.setPadding(new Insets(10));
         return grid;
+    }
+
+    public HBox createButton(String label)    {
+        Button btn = new Button(label);
+        btn.getStyleClass().add("button");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+
+
+            }
+        });
+        return hbBtn;
     }
 }
