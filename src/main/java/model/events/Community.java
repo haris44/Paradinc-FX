85772904787c8103ad92761b6c40892723a9edb5
haris44 <Haris44350>
2017-01-30 17:31:44 +0100

    create fake event

---
+++ src/main/java/model/events/Community.java
@@ -0,0 +1,29 @@
+package model.events;
+
+import javafx.util.Pair;
+import map.ParadincRegion;
+import model.Game;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 30/01/2017.
+ */
+public class Community extends Event {
+
+	Pair<Integer, Double> contamination;
+
+	public Community(String nom, int price, int lenghtStars, int lenghtContamination, Pair<Integer, Double> contamination) {
+		super(nom, price, lenghtStars, lenghtContamination);
+		this.contamination = contamination;
+	}
+
+	@Override
+	public ThrowableEvent getThrowable(Game game) {
+		ArrayList<Integer> proba = new ArrayList<>();
+		proba.add(processAttribute(contamination, game.getRegionController().getGlobalContamination()));
+		ParadincRegion regions = game.getRegionController().pickRegions();
+		return new ThrowableEvent(this, 1, this.getMoyenne(proba), price, regions);
+
+	}
+}
---
+++ src/main/java/model/events/Evangelist.java
@@ -0,0 +1,28 @@
+package model.events;
+
+import javafx.util.Pair;
+import map.ParadincRegion;
+import model.Game;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 30/01/2017.
+ */
+public class Evangelist extends Event {
+
+	Pair<Integer, Double> contamination;
+
+	public Evangelist(String nom, int price, int lenghtStars, int lenghtContamination, Pair<Integer, Double> contamination) {
+		super(nom, price, lenghtStars, lenghtContamination);
+		this.contamination = contamination;
+	}
+
+	@Override
+	public ThrowableEvent getThrowable(Game game) {
+		ArrayList<Integer> proba = new ArrayList<>();
+		proba.add(processAttribute(contamination, game.getRegionController().getGlobalContamination()));
+		ParadincRegion regions = game.getRegionController().pickRegions();
+		return new ThrowableEvent(this, 1, this.getMoyenne(proba), price, regions);
+	}
+}
